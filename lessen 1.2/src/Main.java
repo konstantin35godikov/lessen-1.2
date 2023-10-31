b61362126732c21f367ceafb62b4df8a38e0e25a
User <martemjanovkonstantin@gmail.com>
2023-10-31 22:52:16 +0600

    Main

---
+++ lessen 1.2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("************************************************");
+        System.out.println("****                  Визитка               ****");
+        System.out.println("****            Name: Константин            ****");
+        System.out.println("**** Skype: martemjanovkonstantin@gmail.com ****");
+        System.out.println("************************************************");
+    }
+}
